01f7773b8e59bc0b6317d6390ecec47c2c82c75c
Imahe ruth <imaheruth@gmail.com>
2024-06-19 11:41:29 +0100

    Created the necessary data transfer objects (DTOs) and models.

---
+++ ApplicationFormApp1/Models/Question.cs
@@ -0,0 +1,27 @@
+﻿namespace ApplicationFormApp1.Models
+{
+    public class Question
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public QuestionType Type { get; set; }
+        public List<string> Options { get; set; }
+    }
+
+    public enum QuestionType
+    {
+        Paragraph,
+        YesNo,
+        Dropdown,
+        MultipleChoice,
+        Date,
+        Number
+    }
+
+    public class QuestionDto
+    {
+        public string Text { get; set; }
+        public QuestionType Type { get; set; }
+        public List<string> Options { get; set; }
+    }
+}
